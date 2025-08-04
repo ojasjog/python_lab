8ee80d068728849015a498d10052106b25397714
Ojas <ojasmjog@gmail.com>
2025-08-04 21:38:36 +0530

    First file in python lab

---
+++ py1.py
@@ -0,0 +1,10 @@
+place=input("Enter restaurant name:")
+
+rating=float(input("Enter rating out of 10:"))
+
+rem=input("Remarks:")
+
+
+print(f"The restaurant was {place}.")
+print(f"I would like to give the restaurant a rating of {rating:.2f}/10")
+print(rem)
