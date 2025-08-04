8e776ed2aeff09bc75ee07c9446aa8ee1d7e9528
sudo_dev <ojasmjog@gmail.com>
2025-08-04 21:42:14 +0530

    added a print message

--- py1.py
+++ py1.py
@@ -8,3 +8,8 @@ rem=input("Remarks:")
 print(f"The restaurant was {place}.")
 print(f"I would like to give the restaurant a rating of {rating:.2f}/10")
 print(rem)
+
+
+
+
+print(this is a commit from git web)
