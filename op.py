2df34e9db3fd94ac3dd8feaf20efa26e5cee9d6a
Ojas <ojasmjog@gmail.com>
2025-08-07 18:26:17 +0530

    quadratic project

---
+++ op.py
@@ -0,0 +1,15 @@
+# You are using Python
+a=int(input())
+b=int(input())
+c=int(input())
+
+D=(b**2)-(4*a*c)
+sum1=(-(-b/a))**2
+prod=(D)*(a+b+c)
+diff=((a**2)+(b**2)+(c**2))-((a+b+c)**2)
+k=int(sum1)
+
+print(D)
+print(k)
+print(prod)
+print(diff)
